c7f457e8d52a514a6d02d4a9357475e6d10c0374
dheurlin <daniel.heurlin@gmail.com>
2023-07-18 23:42:56 +0200

    reorder stuff in decoder example file

--- src/decodeTest.c
+++ src/decodeTest.c
@@ -7,6 +7,11 @@ char *pointStr =
   \"x\": 69,\n\
   \"y\": 420,\n\
 }";
+char *personStr = "{\n  \"firstName\": \"Walter\",\n  \"lastName\" : \"White\",\n  \"age\": 52\n}";
+char *numbersStr = "[1, 2, 3, 4, 5]";
+char *pointListStr = "[{ \"x\": 19, \"y\": 95 }, { \"x\": 4, \"y\": 20 }, { \"x\": 18, \"y\": 99 }]";
+char *familyStr = "{\"father\":{\"firstName\":\"Walter\",\"lastName\":\"White\",\"age\":52},\"mother\":{\"firstName\":\"Skyler\",\"lastName\":\"White\",\"age\":40},\"children\":[{\"firstName\":\"Walter Jr.\",\"lastName\":\"White\",\"age\":17},{\"firstName\":\"Holly\",\"lastName\":\"White\",\"age\":1}]}";
+char *familyStrWrong = "{\"father\":{\"firstName\":\"Walter\",\"lastName\":\"White\",\"age\":52},\"mother\":{\"firstName\":\"Skyler\",\"lastName\":\"White\",\"age\":40},\"children\":[{\"firstName\":\"Walter Jr.\",\"lastName\":\"White\",\"age\":17},{\"firstName\":\"Holly\",\"lastName\":\"White\",\"age\": \"hello\"}]}";
 
 typedef struct Point {
   int x;
@@ -17,7 +22,39 @@ void printPoint(Point point) {
   printf("Point { .x = %d, .y = %d }\n", point.x, point.y);
 }
 
-char *personStr = "{\n  \"firstName\": \"Walter\",\n  \"lastName\" : \"White\",\n  \"age\": 52\n}";
+bool decodePoint(DecoderState *state, void *dest) {
+  Point *point = (Point*)dest;
+  return decodeFields(state, 2, 
+    makeField("x", &point->x, decodeInt),
+    makeField("y", &point->y, decodeInt)
+  );
+}
+
+typedef struct NumberList {
+  int *numbers;
+  int length;
+} NumberList;
+
+bool decodeNumberList(DecoderState *state, void *dest) {
+  NumberList *numberList = (NumberList*)dest;
+  return decodeList(state, &numberList->numbers, &numberList->length, sizeof(int), decodeInt);
+}
+
+void printNumberList(NumberList list) {
+  for (int i = 0; i < list.length; i++) {
+    printf("%d\n", list.numbers[i]);
+  }
+}
+
+typedef struct PointList {
+  Point *points;
+  int len;
+} PointList;
+
+bool decodePointList(DecoderState *state, void *dest) {
+  PointList *pointList = (PointList*)dest;
+  return decodeList(state, &pointList->points, &pointList->len, sizeof(Point), decodePoint);
+}
 
 typedef struct Person {
   char *firstName;
@@ -29,44 +66,12 @@ void printPerson(Person person) {
   printf("%s %s, %d\n", person.firstName, person.lastName, person.age);
 }
 
-char *numbersStr = "[1, 2, 3, 4, 5]";
-
-typedef struct NumberList {
-  int *numbers;
-  int length;
-} NumberList;
-
-void printNumberList(NumberList list) {
-  for (int i = 0; i < list.length; i++) {
-    printf("%d\n", list.numbers[i]);
-  }
-}
-
-char *pointListStr = "[{ \"x\": 19, \"y\": 95 }, { \"x\": 4, \"y\": 20 }, { \"x\": 18, \"y\": 99 }]";
-
-typedef struct PointList {
-  Point *points;
-  int len;
-} PointList;
-
 typedef struct Family {
   Person father;
   Person mother;
   Person *children;
   int childCount;
 } Family;
-
-char *familyStr = "{\"father\":{\"firstName\":\"Walter\",\"lastName\":\"White\",\"age\":52},\"mother\":{\"firstName\":\"Skyler\",\"lastName\":\"White\",\"age\":36},\"children\":[{\"firstName\":\"Walter Jr.\",\"lastName\":\"White\",\"age\":16},{\"firstName\":\"Holly\",\"lastName\":\"White\",\"age\":0}]}";
-
-char *familyStrWrong = "{\"father\":{\"firstName\":\"Walter\",\"lastName\":\"White\",\"age\":52},\"mother\":{\"firstName\":\"Skyler\",\"lastName\":\"White\",\"age\":36},\"children\":[{\"firstName\":\"Walter Jr.\",\"lastName\":\"White\",\"age\":16},{\"firstName\":\"Holly\",\"lastName\":\"White\",\"age\": \"hello\"}]}";
-
-bool decodePoint(DecoderState *state, void *dest) {
-  Point *point = (Point*)dest;
-  return decodeFields(state, 2, 
-    makeField("x", &point->x, decodeInt),
-    makeField("y", &point->y, decodeInt)
-  );
-}
 
 bool decodePerson(DecoderState *state, void *dest) {
   Person *person = (Person*)dest;
@@ -75,16 +80,6 @@ bool decodePerson(DecoderState *state, void *dest) {
     makeField("lastName", &person->lastName, decodeString),
     makeField("age", &person->age, decodeInt)
   );
-}
-
-bool decodeNumberList(DecoderState *state, void *dest) {
-  NumberList *numberList = (NumberList*)dest;
-  return decodeList(state, &numberList->numbers, &numberList->length, sizeof(int), decodeInt);
-}
-
-bool decodePointList(DecoderState *state, void *dest) {
-  PointList *pointList = (PointList*)dest;
-  return decodeList(state, &pointList->points, &pointList->len, sizeof(Point), decodePoint);
 }
 
 bool decodeFamily(DecoderState *state, void *dest) {
@@ -101,6 +96,7 @@ int main() {
   decode(pointStr, &decodedPoint, decodePoint);
 
   printf("Decoded point: \n");
+  printf("----------------------------\n");
   printPoint(decodedPoint);
   printf("\n");
 
@@ -110,6 +106,7 @@ int main() {
   decode(personStr, &decodedPerson, decodePerson);
 
   printf("Decoded person: \n");
+  printf("----------------------------\n");
   printPerson(decodedPerson);
   printf("\n");
 
@@ -118,6 +115,7 @@ int main() {
   decode(numbersStr, &numberList, decodeNumberList);
 
   printf("Decoded list: \n");
+  printf("----------------------------\n");
   printNumberList(numberList);
   printf("\n");
 
@@ -126,6 +124,7 @@ int main() {
   decode(pointListStr, &pointList, decodePointList);
 
   printf("Decoded list of points: \n");
+  printf("----------------------------\n");
   for (int i = 0; i < pointList.len; i++) {
     printPoint(pointList.points[i]);
   }
@@ -142,14 +141,19 @@ int main() {
   }
 
   printf("Decoded family: \n");
+  printf("----------------------------\n");
   printf("Father: "); printPerson(family.father);
   printf("Mother: "); printPerson(family.mother);
   printf("Children: \n");
   for (int i = 0; i < family.childCount; i++) {
     printf("  "); printPerson(family.children[i]);
   }
+  printf("\n");
 
   // --------------
+
+  printf("Error message example: \n");
+  printf("----------------------------\n");
 
   Family wrongFam;
   DecodeResult wrongFamRes = decode(familyStrWrong, &wrongFam, decodeFamily);
